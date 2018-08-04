51f6a7c0eff688c6d4051fc084757eb1b506da73
Eric Hsieh <1102345117@gm.kuas.edu.tw>
2018-08-04 15:44:30 +0800

    Add files via upload

---
+++ php/tcpdf/fonts/barcode.php
@@ -0,0 +1,15 @@
+<?php
+// TCPDF FONT FILE DESCRIPTION
+$type='TrueTypeUnicode';
+$name='USPSBarCode';
+$up=-80;
+$ut=50;
+$dw=155;
+$diff='';
+$originalsize=4324;
+$enc='';
+$file='barcode.z';
+$ctg='barcode.ctg.z';
+$desc=array('Flags'=>32,'FontBBox'=>'[0 0 938 566]','ItalicAngle'=>0,'Ascent'=>1000,'Descent'=>0,'Leading'=>150,'CapHeight'=>566,'XHeight'=>566,'StemV'=>1,'StemH'=>0,'AvgWidth'=>667,'MaxWidth'=>1060,'MissingWidth'=>155);
+$cw=array(0=>155,32=>155,33=>500,34=>500,35=>500,36=>500,37=>500,38=>500,39=>500,40=>500,41=>500,42=>500,43=>500,44=>500,45=>500,46=>500,47=>212,48=>1060,49=>1060,50=>1060,51=>1060,52=>1060,53=>1060,54=>1060,55=>1060,56=>1060,57=>1060,58=>212,57344=>500,65535=>155);
+// --- EOF ---
